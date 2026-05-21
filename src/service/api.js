54ab61ddf911b08fcbf4221a2722d7ef17c64344
unknown <jainyashit11.com>
2026-05-21 23:41:06 +0530

    backend url correction

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api/v1', // Now you only need to write '/auth/verify' instead of the full path
+  baseURL: import.meta.env.BACKEND_URL, // Now you only need to write '/auth/verify' instead of the full path
 });
 
 // REQUEST INTERCEPTOR: Automatically attach the JWT to every request
